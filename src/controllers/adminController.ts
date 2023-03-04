448c482b981c309d88f8b8eaa629fe3d26fd57f4
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-03-04 14:38:09 +0000

    feat: check if admin exists in database

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,10 +1,25 @@
 import { Request, Response } from "express"
-import { validationError } from "../utils/validator"
-export const postLoginAdmin = (req:Request, res:Response)=>{
-  const _err = validationError(req) 
-  console.log(_err)
-    // check if email && password match 
-    
+import constants from "../constants"
+import User, { ROLE } from "../models/User"
+import UserModel from "../models/User"
 
-    // return error if not match
+/**
+ * @todo: Wrap in Try Catch
+ *        Check Password
+ *        Encrypt & Decrypt Password
+ **/
+
+export const postLoginAdmin = async(req:Request, res:Response)=>{
+  // check if email and isADmin
+ const user =  await UserModel.findOne({where:{
+  email:req.body.email,
+  role:ROLE.admin 
+ }})
+ if(user){
+  // check password
+    console.log(user)
+   return res.status(constants.status.ok).json({msg:""})
+  }
+  // return error if not match
+ return res.status(constants.status.notFound).json({msg:"Credentials not Found"})
 }
