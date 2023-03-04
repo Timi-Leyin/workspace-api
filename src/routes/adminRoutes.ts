8f4e78b40e331ffc2bd774999ca6c8e4722ddd46
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-03-04 14:39:25 +0000

    fix: validation imports

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,10 +1,10 @@
 import express from "express"
 import constants from "../constants"
 import { postLoginAdmin } from "../controllers/adminController"
-import { adminLoginValidator } from "../utils/validator"
+import { adminLoginValidator, validationError } from "../middlewares/validator"
 const route = express.Router()
 
-route.post(constants.routes.admin.login,  adminLoginValidator, postLoginAdmin )
+route.post(constants.routes.admin.login,  adminLoginValidator, validationError, postLoginAdmin )
 
 
 export default route
