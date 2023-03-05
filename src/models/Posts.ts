eb5125fcc11c01e35321bcc2c0ba41151d28ac22
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-03-05 20:15:48 +0000

    feat: add views

--- src/models/Posts.ts
+++ src/models/Posts.ts
@@ -1,51 +1,54 @@
 import { DataTypes } from "sequelize";
 import db from "../config/database";
 
-
-const PostModel = db.define("posts",{
-    id:{
-        type:DataTypes.INTEGER,
-        allowNull:false,
-        primaryKey:true,
-        unique:true,
-        autoIncrement:true
-    },
-    user_id:{
-        type:DataTypes.STRING,
-    },
-    title:{
-        type:DataTypes.STRING,
-        allowNull:false
-    },
-    category:{
-        type:DataTypes.STRING,
-        defaultValue:"",
-    },
-    content:{
-        type:DataTypes.TEXT,
-        allowNull:false
-    },
-    thumbnail:{
-        type:DataTypes.STRING,
-    },
-    meta_title:{
-        type:DataTypes.STRING,
-    },
-    meta_description:{
-        type:DataTypes.STRING,
-    },
-    meta_keywords:{
-        type:DataTypes.STRING,
-    },
-    meta_robots:{
-        type:DataTypes.BOOLEAN,
-    },
-    uuid:{
-        type:DataTypes.STRING,
-        allowNull:false
-    },
-    archived:{
-        type:DataTypes.BOOLEAN,
-        defaultValue:false
-    }
-})
+const PostModel = db.define("posts", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    unique: true,
+    autoIncrement: true,
+  },
+  user_id: {
+    type: DataTypes.STRING,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    defaultValue: "",
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  thumbnail: {
+    type: DataTypes.STRING,
+  },
+  views: {
+    type: DataTypes.INTEGER,
+    defaultValue:0
+  },
+  meta_title: {
+    type: DataTypes.STRING,
+  },
+  meta_description: {
+    type: DataTypes.STRING,
+  },
+  meta_keywords: {
+    type: DataTypes.STRING,
+  },
+  meta_robots: {
+    type: DataTypes.BOOLEAN,
+  },
+  uuid: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  archived: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
