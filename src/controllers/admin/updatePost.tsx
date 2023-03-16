1c52553904219496915ccc8dd0d0c94641928e25
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-03-16 12:15:24 +0100

    feat: update post controller

--- src/controllers/admin/updatePost.tsx
+++ src/controllers/admin/updatePost.tsx
@@ -1,4 +1,41 @@
 import { Request, Response } from "express";
-const updatePost = async (req: Request, res: Response) => {};
+import constants from "../../constants";
+import PostModel from "../../models/Posts";
+const updatePost = async (req: Request, res: Response) => {
+  const postId = req.params.id;
+  const {
+    title,
+    thumbnail,
+    category,
+    content,
+    meta_title,
+    meta_decription,
+    meta_keywords,
+    meta_robots,
+    archived,
+  } = req.body;
+
+  console.log(postId);
+
+  const postInfo = await PostModel.update(
+    {
+      title,
+    },
+    {
+      where: {
+        uuid: postId,
+      },
+    }
+  );
+
+  if (postInfo[0]) {
+    return res.status(constants.status.ok).json({
+      msg: "Post Updated 👍",
+    });
+  }
+  return res.status(constants.status.notAcceptable).send({
+    msg: "Could not Update Post",
+  });
+};
 
 export default updatePost;
