9d7d9048b53d01d2b46e915bd4d765e33dabdbd1
Timi-Leyin <timileyinoyelekan11@gmail.com>
2023-03-16 12:15:34 +0100

    feat: delete post controller

--- src/controllers/admin/deletePost.ts
+++ src/controllers/admin/deletePost.ts
@@ -4,14 +4,13 @@ import { Request, Response } from "express";
 
 const deletePost = async (req: Request, res: Response) => {
   const postID = req.params.id;
-
   const post = await PostModel.destroy({
     where: {
       uuid: postID,
     },
   });
   if (post) return res.status(constants.status.ok).json({ msg: "Deleted" });
-  return res.status(constants.status.noContent).json({});
+return res.status(constants.status.conflict).json({ msg: "Post Not Found" });
 };
 
 export default deletePost;
